f5bca5acd47a96d15ef968582602990a94d992a8
chhetribishal <bishal.in.thuma012@gmail.com>
2022-07-30 23:45:33 +0545

    "vb commit"

---
+++ Patient.Designer.vb
@@ -0,0 +1,213 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Patient
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.GroupBox1 = New System.Windows.Forms.GroupBox()
+        Me.Button6 = New System.Windows.Forms.Button()
+        Me.Button5 = New System.Windows.Forms.Button()
+        Me.Button4 = New System.Windows.Forms.Button()
+        Me.Button3 = New System.Windows.Forms.Button()
+        Me.Insert = New System.Windows.Forms.Button()
+        Me.Button1 = New System.Windows.Forms.Button()
+        Me.txtSearch = New System.Windows.Forms.TextBox()
+        Me.txtAge = New System.Windows.Forms.TextBox()
+        Me.txtSurname = New System.Windows.Forms.TextBox()
+        Me.txtName = New System.Windows.Forms.TextBox()
+        Me.Label4 = New System.Windows.Forms.Label()
+        Me.Label3 = New System.Windows.Forms.Label()
+        Me.Label2 = New System.Windows.Forms.Label()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.GroupBox1.SuspendLayout()
+        Me.SuspendLayout()
+        '
+        'GroupBox1
+        '
+        Me.GroupBox1.Controls.Add(Me.Button6)
+        Me.GroupBox1.Controls.Add(Me.Button5)
+        Me.GroupBox1.Controls.Add(Me.Button4)
+        Me.GroupBox1.Controls.Add(Me.Button3)
+        Me.GroupBox1.Controls.Add(Me.Insert)
+        Me.GroupBox1.Controls.Add(Me.Button1)
+        Me.GroupBox1.Controls.Add(Me.txtSearch)
+        Me.GroupBox1.Controls.Add(Me.txtAge)
+        Me.GroupBox1.Controls.Add(Me.txtSurname)
+        Me.GroupBox1.Controls.Add(Me.txtName)
+        Me.GroupBox1.Controls.Add(Me.Label4)
+        Me.GroupBox1.Controls.Add(Me.Label3)
+        Me.GroupBox1.Controls.Add(Me.Label2)
+        Me.GroupBox1.Controls.Add(Me.Label1)
+        Me.GroupBox1.Location = New System.Drawing.Point(0, 0)
+        Me.GroupBox1.Name = "GroupBox1"
+        Me.GroupBox1.Size = New System.Drawing.Size(705, 419)
+        Me.GroupBox1.TabIndex = 0
+        Me.GroupBox1.TabStop = False
+        Me.GroupBox1.Text = "PatientDetails"
+        '
+        'Button6
+        '
+        Me.Button6.Location = New System.Drawing.Point(426, 236)
+        Me.Button6.Name = "Button6"
+        Me.Button6.Size = New System.Drawing.Size(72, 29)
+        Me.Button6.TabIndex = 13
+        Me.Button6.Text = "Search"
+        Me.Button6.UseVisualStyleBackColor = True
+        '
+        'Button5
+        '
+        Me.Button5.Location = New System.Drawing.Point(484, 307)
+        Me.Button5.Name = "Button5"
+        Me.Button5.Size = New System.Drawing.Size(94, 29)
+        Me.Button5.TabIndex = 12
+        Me.Button5.Text = "Drop Table"
+        Me.Button5.UseVisualStyleBackColor = True
+        '
+        'Button4
+        '
+        Me.Button4.Location = New System.Drawing.Point(372, 307)
+        Me.Button4.Name = "Button4"
+        Me.Button4.Size = New System.Drawing.Size(94, 29)
+        Me.Button4.TabIndex = 11
+        Me.Button4.Text = "Delete Data"
+        Me.Button4.UseVisualStyleBackColor = True
+        '
+        'Button3
+        '
+        Me.Button3.Location = New System.Drawing.Point(258, 307)
+        Me.Button3.Name = "Button3"
+        Me.Button3.Size = New System.Drawing.Size(94, 29)
+        Me.Button3.TabIndex = 10
+        Me.Button3.Text = "Update Data"
+        Me.Button3.UseVisualStyleBackColor = True
+        '
+        'Insert
+        '
+        Me.Insert.Location = New System.Drawing.Point(144, 307)
+        Me.Insert.Name = "Insert"
+        Me.Insert.Size = New System.Drawing.Size(94, 29)
+        Me.Insert.TabIndex = 9
+        Me.Insert.Text = "Insert Data"
+        Me.Insert.UseVisualStyleBackColor = True
+        '
+        'Button1
+        '
+        Me.Button1.Location = New System.Drawing.Point(12, 307)
+        Me.Button1.Name = "Button1"
+        Me.Button1.Size = New System.Drawing.Size(113, 29)
+        Me.Button1.TabIndex = 8
+        Me.Button1.Text = "Create Table"
+        Me.Button1.UseVisualStyleBackColor = True
+        '
+        'txtSearch
+        '
+        Me.txtSearch.Location = New System.Drawing.Point(92, 236)
+        Me.txtSearch.Name = "txtSearch"
+        Me.txtSearch.Size = New System.Drawing.Size(311, 27)
+        Me.txtSearch.TabIndex = 7
+        '
+        'txtAge
+        '
+        Me.txtAge.Location = New System.Drawing.Point(92, 164)
+        Me.txtAge.Name = "txtAge"
+        Me.txtAge.Size = New System.Drawing.Size(406, 27)
+        Me.txtAge.TabIndex = 6
+        '
+        'txtSurname
+        '
+        Me.txtSurname.Location = New System.Drawing.Point(92, 94)
+        Me.txtSurname.Name = "txtSurname"
+        Me.txtSurname.Size = New System.Drawing.Size(406, 27)
+        Me.txtSurname.TabIndex = 5
+        '
+        'txtName
+        '
+        Me.txtName.Location = New System.Drawing.Point(92, 39)
+        Me.txtName.Name = "txtName"
+        Me.txtName.Size = New System.Drawing.Size(406, 27)
+        Me.txtName.TabIndex = 4
+        '
+        'Label4
+        '
+        Me.Label4.AutoSize = True
+        Me.Label4.Location = New System.Drawing.Point(12, 236)
+        Me.Label4.Name = "Label4"
+        Me.Label4.Size = New System.Drawing.Size(53, 20)
+        Me.Label4.TabIndex = 3
+        Me.Label4.Text = "Search"
+        '
+        'Label3
+        '
+        Me.Label3.AutoSize = True
+        Me.Label3.Location = New System.Drawing.Point(12, 164)
+        Me.Label3.Name = "Label3"
+        Me.Label3.Size = New System.Drawing.Size(36, 20)
+        Me.Label3.TabIndex = 2
+        Me.Label3.Text = "Age"
+        '
+        'Label2
+        '
+        Me.Label2.AutoSize = True
+        Me.Label2.Location = New System.Drawing.Point(12, 94)
+        Me.Label2.Name = "Label2"
+        Me.Label2.Size = New System.Drawing.Size(67, 20)
+        Me.Label2.TabIndex = 1
+        Me.Label2.Text = "Surname"
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(12, 39)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(49, 20)
+        Me.Label1.TabIndex = 0
+        Me.Label1.Text = "Name"
+        '
+        'Patient
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(8.0!, 20.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(1294, 510)
+        Me.Controls.Add(Me.GroupBox1)
+        Me.Name = "Patient"
+        Me.Text = "PatientInformation"
+        Me.GroupBox1.ResumeLayout(False)
+        Me.GroupBox1.PerformLayout()
+        Me.ResumeLayout(False)
+
+    End Sub
+
+    Friend WithEvents GroupBox1 As GroupBox
+    Friend WithEvents Button6 As Button
+    Friend WithEvents Button5 As Button
+    Friend WithEvents Button4 As Button
+    Friend WithEvents Button3 As Button
+    Friend WithEvents Insert As Button
+    Friend WithEvents Button1 As Button
+    Friend WithEvents txtSearch As TextBox
+    Friend WithEvents txtAge As TextBox
+    Friend WithEvents txtSurname As TextBox
+    Friend WithEvents txtName As TextBox
+    Friend WithEvents Label4 As Label
+    Friend WithEvents Label3 As Label
+    Friend WithEvents Label2 As Label
+    Friend WithEvents Label1 As Label
+End Class
---
+++ Patient.vb
@@ -0,0 +1,47 @@
+﻿Imports System.Data.SqlClient  'Import SQL capabilities
+
+Public Class Patient
+    Private strConn As String = "Data Source=DESKTOP-BPNBOGT;Initial Catalog=master;Integrated Security=True"
+    Private sqlCon As SqlConnection
+    Private Sub Patient_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+
+        Dim strQuery As String
+        strQuery = "Select * From PatientTbl"
+        sqlCon = New SqlConnection(strConn)
+
+        Using (sqlCon)
+            Dim SqlCmd As SqlCommand = New SqlCommand(strQuery, sqlCon)
+            sqlCon.Open()
+
+            Dim SqlReader As SqlDataReader = SqlCmd.ExecuteReader()
+
+            If SqlReader.HasRows Then
+                While (SqlReader.Read())
+                    txtName.Text = SqlReader.GetString(1)
+                    txtSurname.Text = SqlReader.GetString(2)
+                    txtAge.Text = SqlReader.GetValue(3)
+                End While
+            End If
+            SqlReader.Close()
+        End Using
+    End Sub
+
+
+    'CreateTable
+    Private Sub Button1_Click(sender As Object, e As EventArgs) Handles Button1.Click
+        sqlCon = New SqlConnection(strConn)
+        Using (sqlCon)
+            Dim SqlCmd As New SqlCommand
+            SqlCmd.Connection = sqlCon
+            SqlCmd.CommandText = "sp_CreateTable"
+            SqlCmd.CommandType = CommandType.StoredProcedure
+
+            sqlCon.Open()
+            SqlCmd.ExecuteNonQuery()
+        End Using
+    End Sub
+
+    Private Sub Insert_Click(sender As Object, e As EventArgs) Handles Insert.Click
+
+    End Sub
+End Class
